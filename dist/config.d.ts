0a0fc94dce1dc0a2b6bb8920281df290d25cc69b
Tom Lee <thomasslee97@gmail.com>
2020-04-14 12:18:19 +0100

    chore: dist: Table column ranks

--- dist/config.d.ts
+++ dist/config.d.ts
@@ -35,6 +35,9 @@ export interface ConfigManifestEntryEnum extends ConfigManifestEntryBase {
 }
 export interface ConfigManifestEntryTable extends ConfigManifestEntryBase {
     type: ConfigManifestEntryType.TABLE;
-    columns: BasicConfigManifestEntry[];
+    columns: Array<BasicConfigManifestEntry & {
+        /** Column rank (left to right, lowest to highest) */
+        rank: number;
+    }>;
     defaultVal: TableConfigItemValue;
 }
